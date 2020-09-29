5ab4c3aacbc3b8c61f8471464b20f3fb59e9758e
Vitaly Vysotsky <VitalyVysotsky@coherentsolutions.com>
2020-09-29 19:36:00 +0300

    after creating chickgramm pug searching

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,34 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync');
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');//  минификация JS файлов
+const concat = require('gulp-concat');// Позволяет обьединять JS файлы
 const rename = require('gulp-rename');
 const pug = require('gulp-pug');
 
+gulp.task('conosole', function() {
+    console.log('Hello World!')
+})
+
+// gulp.task('default', function() {
+//     return gulp.src(['app/html/*', 'app/css/*'], {read: false})//Второй параметр - настпройки, с какими принимаются файлы
+//         .on('data', function(file) {// прослушка события и ввывод в консоль измененных файлов принимает файл, над которым идет работа
+//             return 'dest'//можно сразу задать выходной путь
+//             console.log(file)
+//         })
+//         .pipe(gulp.dest('tmp'))
+// })
+
 gulp.task('scss', function() {
-    return gulp.src('app/scss/**/*.scss')
+    return gulp.src('app/scss/**/*.scss')//Входной путь
         .pipe(sass({outputStyle: 'expanded'}))//compressed
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('app/css'))
+        .pipe(gulp.dest('app/css'))// путь перемещения
         .pipe(browserSync.reload({stream: true}))
 });
 
 // gulp.task('html', function() {
-//     return gulp.src('app/*.html')
+//     return gulp.src('app/*.html') 
 //         .pipe(browserSync.reload({steram: true}))
 // })
 
@@ -30,7 +43,7 @@ gulp.task('scss', function() {
 
 gulp.task('pug', function() {
     return gulp.src('app/pug/*.pug')
-        .pipe(pug())
+        .pipe(pug({pretty: true}))//инлайновые элементы верстки перекомпилируются в нормальную форму, а не инлайново
         .pipe(gulp.dest('app/html'))
         .pipe(browserSync.reload({stream: true}))
 
@@ -52,6 +65,8 @@ gulp.task('watch', function() {
 gulp.task('browser-sync', function() {
     browserSync.init({server: { baseDir: 'app/html'}})
 });
+
+// gulp.task('default', ['pug', 'scss', 'js'])
 
 gulp.task('default', gulp.parallel(
     'pug',
